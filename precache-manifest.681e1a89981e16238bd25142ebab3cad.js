7d0342fe151a87ad4a0bf9dd72dbfdee1f22bf81
Daniel Skowron <skofikit@gmail.com>
2019-04-21 13:47:03 +0200

    Updates

---
+++ precache-manifest.681e1a89981e16238bd25142ebab3cad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "41f84ea320a19e4e9fc2",
+    "url": "/Gallery-3D/static/js/runtime~main.56eb9bbd.js"
+  },
+  {
+    "revision": "7b9c940da82e08c6a2de",
+    "url": "/Gallery-3D/static/js/main.2593abd0.chunk.js"
+  },
+  {
+    "revision": "aaee165f0e99a0a78cc2",
+    "url": "/Gallery-3D/static/js/2.e678367a.chunk.js"
+  },
+  {
+    "revision": "7b9c940da82e08c6a2de",
+    "url": "/Gallery-3D/static/css/main.c75aa451.chunk.css"
+  },
+  {
+    "revision": "c341278319b01b6f2d3e3c13b30699ad",
+    "url": "/Gallery-3D/index.html"
+  }
+];
